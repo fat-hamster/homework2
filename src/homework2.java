1cb57008c0f12a81874fff6631345e209ca54015
Dmitriy Gridchin <tigris.tstu@gmail.com>
2020-11-26 23:13:17 +0300

    Выполнено 1е задание

--- src/homework2.java
+++ src/homework2.java
@@ -1,5 +1,14 @@
+import java.util.Arrays;
+
 public class homework2 {
     public static void main(String[] args) {
-        System.out.println("Prepare to work...");
+        // Задание #1
+        int[] arr1 = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        for (int i = 0; i < arr1.length; i++)
+            arr1[i] = arr1[i] == 1 ? 0 : 1;
+        System.out.println(Arrays.toString(arr1));
+
+        // Задание #2
+
     }
 }
