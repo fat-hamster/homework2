0a9f298cfc9b232f9e999f3e54d321fd530df0b5
Dmitriy Gridchin <tigris.tstu@gmail.com>
2020-11-27 15:37:40 +0300

    Сделаны все задания кроме 6-го

--- src/homework2.java
+++ src/homework2.java
@@ -1,14 +1,159 @@
 import java.util.Arrays;
+import java.util.Random;
 
 public class homework2 {
     public static void main(String[] args) {
         // Задание #1
         int[] arr1 = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        System.out.println("Задание #1");
+        System.out.printf("Исходный массив:%n%s%n", Arrays.toString(arr1));
         for (int i = 0; i < arr1.length; i++)
             arr1[i] = arr1[i] == 1 ? 0 : 1;
-        System.out.println(Arrays.toString(arr1));
+        System.out.printf("Измененный массив%n%s%n%n", Arrays.toString(arr1));
 
         // Задание #2
+        int[] arr2 = new int[8];
+        int val = 0;
+        for (int i = 0; i < arr2.length; i++) {
+            arr2[i] = val;
+            val+=3;
+        }
+        System.out.printf("Задание #2%n%s%n%n", Arrays.toString(arr2));
 
+        //Задание #3
+        int[] arr3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        System.out.println("Задание #3");
+        System.out.printf("Исходный массив%n%s%n", Arrays.toString(arr3));
+        for (int i = 0; i < arr3.length; i++) {
+            if(arr3[i] < 6)
+                arr3[i] *= 2;
+        }
+        System.out.printf("Измененный массив%n%s%n%n", Arrays.toString(arr3));
+
+        // Задание #4
+        int[][] square = new int[3][3];
+        for (int i = 0; i < square.length; i++)
+            square[i][i] = 1;
+        System.out.println("Задание #4");
+        System.out.println("Заполненный массив");
+        for (int[] ints : square) {
+            for (int j = 0; j < square.length; j++) { // Использую это значение т.к. массив квадратный
+                System.out.printf("%d ", ints[j]);
+            }
+            System.out.println();
+        }
+        System.out.println(); // Зрительно отделяю задания
+
+        // Задание #5
+        int[] arr4 = new int[20];
+        int diff = 200; // Ограниечение для генерации чисел
+        Random rnd = new Random();
+        for (int i = 0; i < arr4.length; i++)
+            arr4[i] = rnd.nextInt(diff + 1);            // Заполняем массив псевдослучайными числами
+        System.out.println("Задание #5");
+        System.out.printf("Исходный массив%n%s%n", Arrays.toString(arr4));
+        int min = arr4[0];  // Присваиваю первый элемент массива
+        int max = arr4[0];  // так как пофиг какой :)
+        for (int j : arr4) {
+            if (j > max)
+                max = j;
+            if (j < min)
+                min = j;
+        }
+        System.out.printf("Минимальное значение в массиве: %d%n", min);
+        System.out.printf("Максимальное значение в массиве: %d%n%n", max);
+
+        // Задание #6
+        System.out.println("Задание #6");
+        int[] arr5 = {3, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        System.out.println(checkBalance(arr5));
+
+        // Задание #7
+        int[] arr6 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        System.out.println("Задание #7");
+        System.out.println("Исходный сассив");
+        System.out.println(Arrays.toString(arr6));
+        System.out.println("Массив после сдвига");
+        if(arr6.length < 100)
+            System.out.println(Arrays.toString(shiftArray(arr6, 7)));
+        else
+            System.out.println(Arrays.toString(shiftBigArray(arr6, 7)));
+    }
+
+    private static boolean checkBalance(int[] arr) {
+        if(arr.length < 4)
+            return false; // потому что не получится суммы правой и левой половины массива
+        int leftSum = 0;
+        int rightSum = 0;
+
+        leftSum = arr[0] + arr[1];
+        rightSum = arr[arr.length - 2] + arr[arr.length - 1];
+        if(arr.length == 4) {
+            if (leftSum == rightSum)
+                return true;
+            else
+                return false;
+        }
+
+        
+
+        return false;
+    }
+
+    private static int[] shiftArray(int[] arr, int shift) {
+        if(shift == 0)
+            return arr;
+        if(shift % arr.length == 0)
+            return arr;
+
+        int sh = shift % arr.length;
+
+        if(shift < 0)
+           sh = arr.length - Math.abs(sh);
+
+        for (int i = 0; i < sh; i++) {
+            int last = arr[arr.length - 1];
+            for (int j = arr.length - 1; j > 0; j--) {
+                arr[j] = arr[j-1];
+            }
+            arr[0] = last;
+        }
+
+        return arr;
+    }
+
+    // Этот метод реализован для больших массивов в попытке оптимизировать сдвиг.
+    private static int[] shiftBigArray(int[] arr, int shift) {
+        if(shift == 0)
+            return arr;
+        if(shift % arr.length == 0)
+            return arr;
+
+        int sh = shift % arr.length;
+        if(Math.abs(sh) > arr.length / 2) { // если сдвиг больше половины массива,
+                                            // то быстрее сдвинуть массив в другую сторону
+            sh = arr.length - Math.abs(sh);
+            if(shift > 0)
+                sh *= -1;
+        }
+        if(sh > 0) {
+            for (int i = 0; i < sh; i++) {
+                int last = arr[arr.length - 1];
+                for (int j = arr.length - 1; j > 0; j--) {
+                    arr[j] = arr[j-1];
+                }
+                arr[0] = last;
+            }
+        } else {
+            for (int i = 0; i > sh; i--) {
+                int last = arr[0];
+                for (int j = 0; j < arr.length - 1; j++) {
+                    arr[j] = arr[j+1];
+                }
+                arr[arr.length-1] = last;
+            }
+        }
+
+        return arr;
     }
 }
